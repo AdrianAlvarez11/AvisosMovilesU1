dd5bf9ed2a2415eb426eb43ff2ea1c29b35a9196
Adrian Alvarez <adrianalvarezdev@gmail.com>
2026-04-22 14:06:55 -0600

    cambie el login para que use un solo endpoint y un solo dto, y modifique el jwt para que estuviera igual que el de clase

--- AvisosAPI/Models/DTOs/AuthDTOs.cs
+++ AvisosAPI/Models/DTOs/AuthDTOs.cs
@@ -2,13 +2,7 @@
 {
     public class AuthDTOs
     {
-        public class AlumnoLoginDTO
-        {
-            public string NumControl { get; set; } = null!;
-            public string Contrasena { get; set; } = null!;
-        }
-
-        public class MaestroLoginDTO
+        public class LoginDTO
         {
             public string NumControl { get; set; } = null!;
             public string Contrasena { get; set; } = null!;
@@ -16,7 +10,7 @@
 
         // Lo que regresa la API si el login fue exitoso
         // El Token es el JWT que la app guarda y manda en cada request
-        public class AlumnoLoginResponseDTO
+        public class LoginResponseDTO
         {
             public int Id { get; set; }
             public string NumControl { get; set; } = null!;
@@ -24,16 +18,7 @@
             public int IdGrupo { get; set; }
             public string NombreGrupo { get; set; } = null!;
             public string Token { get; set; } = null!;
-        }
-
-        public class MaestroLoginResponseDTO
-        {
-            public int Id { get; set; }
-            public string NumControl { get; set; } = null!;
-            public string Nombre { get; set; } = null!;
-            public int IdGrupo { get; set; }
-            public string NombreGrupo { get; set; } = null!;
-            public string Token { get; set; } = null!;
+            public string Rol { get; set; } = null!;
         }
     }
 }
--- AvisosAPI/Validators/AuthValidators.cs
+++ AvisosAPI/Validators/AuthValidators.cs
@@ -3,25 +3,14 @@ using static AvisosAPI.Models.DTOs.AuthDTOs;
 
 namespace AvisosAPI.Validators
 {
-    public class AlumnoLoginValidator : AbstractValidator<AlumnoLoginDTO>
+    public class LoginValidator : AbstractValidator<LoginDTO>
     {
-        public AlumnoLoginValidator()
+        public LoginValidator()
         {
             RuleFor(x => x.NumControl)
-                .NotEmpty().WithMessage("El número de control es requerido.");
-            
-            RuleFor(x => x.Contrasena)
-                .NotEmpty().WithMessage("La contraseña es requerida.");
-        }
-    }
+                .NotEmpty().WithMessage("El número de control es requerido.")
+                .Must(x => x.Length == 4 || x.Length == 8).WithMessage("El número de control es requerido.");
 
-    public class MaestroLoginValidator : AbstractValidator<MaestroLoginDTO>
-    {
-        public MaestroLoginValidator()
-        {
-            RuleFor(x => x.NumControl)
-                .NotEmpty().WithMessage("El número de control es requerido.");
-                
             RuleFor(x => x.Contrasena)
                 .NotEmpty().WithMessage("La contraseña es requerida.");
         }
