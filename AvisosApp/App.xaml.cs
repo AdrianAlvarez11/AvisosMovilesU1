b7883283e2eb65f832af8f8cc474f08126c9e62e
KevinFGRM <kevinre14@outlook.com>
2026-04-23 13:42:49 -0600

    cambios de navegacion

--- AvisosApp/App.xaml.cs
+++ AvisosApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AvisosApp.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AvisosApp
 {
@@ -7,6 +8,10 @@ namespace AvisosApp
         public App()
         {
             InitializeComponent();
+            Routing.RegisterRoute("registrarAlumno", typeof(RegisterAlumnoView));
+            Routing.RegisterRoute("crearavisogeneral", typeof(ProfesorCrearAvisoGeneralView));
+            Routing.RegisterRoute("crearavisopersonal", typeof(ProfesorCrearAvisoPersonalView));
+            Routing.RegisterRoute("detallesalumnos", typeof(ProfesorDetallesAlumnosView));
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
--- AvisosApp/ViewModels/AvisosViewModel.cs
+++ AvisosApp/ViewModels/AvisosViewModel.cs
@@ -61,10 +61,10 @@ namespace AvisosApp.ViewModels
             VerAvisosPersonalesCommand = new Command<int>(VerDetalleAvisosPersonales);
             EliminarAvisosPersonalesCommand = new Command<int>(EliminarAvisosPersonales);
 
-            IrCrearAvisoGeneralCommand = new Command(() => Shell.Current.GoToAsync("//crearavisogeneral"));
+            IrCrearAvisoGeneralCommand = new Command(() => Shell.Current.GoToAsync("crearavisogeneral"));
             CrearAvisoGeneralCommand = new Command(CrearAvisoGeneral);
 
-            IrCrearAvisoPersonalCommand = new Command(() => Shell.Current.GoToAsync("//crearavisopersonal"));
+            IrCrearAvisoPersonalCommand = new Command(CambiarACrearAvisoPersonal);
             CrearAvisoPersonalCommand = new Command(CrearAvisoPersonal);
 
             CargarAvisosGeneralesCommand = new Command(CargarAvisosGenerales);
@@ -83,8 +83,13 @@ namespace AvisosApp.ViewModels
                 MostrarGrupo = false;
                 CargarAvisosGenerales();
             });
+
         }
 
+        public void CambiarACrearAvisoPersonal()
+        {
+            Shell.Current.GoToAsync("crearavisopersonal");
+        }
         private void Logout()
         {
             service.Logout();
@@ -171,10 +176,10 @@ namespace AvisosApp.ViewModels
         {
             var alumno = await service.GetAlumno(id);
             if (alumno != null)
-            {
+            {  
                 AlumnoSeleccionado = alumno;
                 PropertyChanged?.Invoke(this, new(nameof(AlumnoSeleccionado)));
-                await Shell.Current.GoToAsync("//detallesalumnos");
+                await Shell.Current.GoToAsync("detallesalumnos");
 
             }
         }
